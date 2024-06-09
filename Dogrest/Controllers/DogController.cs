e43aef82eed5bf751e3d21cbb2673d0402ca8895
Mohamad Kassem <moha226i@edu.zealand.dk>
2024-06-09 16:50:44 +0200

    adding methodes

--- Dogrest/Controllers/DogController.cs
+++ Dogrest/Controllers/DogController.cs
@@ -46,7 +46,7 @@ namespace Dogrest.Controllers
         {
             Dog? dog = _dogsRepository.GetById(id); // her henter vi en hund fra vores repository
             if 
-                (dog == null)
+                (dog is null)
             {
                 return NotFound(); // hvis hunden ikke findes, returnerer vi en 404
             }
@@ -90,6 +90,67 @@ namespace Dogrest.Controllers
                 _dogsRepository.Delete(id); // ellers sletter vi hunden fra vores repository
                 return Ok(dog); // og returnerer en 200 med hunden
             }
+        }
+
+        [HttpGet]
+        [Route("sortbyage")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<IEnumerable<Dog>> SortByAge()
+        {
+            IEnumerable<Dog> dogs = _dogsRepository.SortByAge(); // her henter vi alle hunde fra vores repository
+            // IEnumerable betyder egentlig bare at det er en liste af noget
+            if (dogs.Count() == 0)
+            {
+                return NoContent(); // hvis der ikke er nogen hunde i vores repository, returnerer vi en 204
+            }
+            else
+            {
+                return Ok(dogs); // ellers returnerer vi en 200 med listen af hunde
+
+            }
+
+        }
+        
+        [HttpGet]
+        [Route("sortbyname")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<IEnumerable<Dog>> SortByName()
+        {
+            IEnumerable<Dog> dogs = _dogsRepository.SortByName(); // her henter vi alle hunde fra vores repository
+            // IEnumerable betyder egentlig bare at det er en liste af noget
+            if (dogs.Count() == 0)
+            {
+                return NoContent(); // hvis der ikke er nogen hunde i vores repository, returnerer vi en 204
+            }
+            else
+            {
+                return Ok(dogs); // ellers returnerer vi en 200 med listen af hunde
+
+            }
+
+        }
+
+        [HttpGet]
+        [Route("filterbyname")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<IEnumerable<Dog>> FilterByName([FromQuery] string name)
+        {
+            IEnumerable<Dog> dogs = _dogsRepository.FilterByName(name); // her henter vi alle hunde fra vores repository
+            // IEnumerable betyder egentlig bare at det er en liste af noget
+            if (dogs.Count() == 0)
+            {
+                return NoContent(); // hvis der ikke er nogen hunde i vores repository, returnerer vi en 204
+            }
+            else
+            {
+                return Ok(dogs); // ellers returnerer vi en 200 med listen af hunde
+
+            }
+
+
         }
     }
 }
